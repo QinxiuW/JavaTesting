2bbc683ab4a2ad37fade1f240ed8f091dcc3823b
qwang <qwang@xmltravelgate.com>
2021-07-07 08:09:15 +0200

    resolved version deploy issue

--- wildflyweb/src/main/java/com/web/wildfly/ServletInitializer.java
+++ wildflyweb/src/main/java/com/web/wildfly/ServletInitializer.java
@@ -2,6 +2,7 @@ package com.web.wildfly;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 
 /**
